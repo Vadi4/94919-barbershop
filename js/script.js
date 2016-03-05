9c2d827290e95e1a4690ee21f276cf38b20d424e
Vadi4 <Vadich92@gmail.com>
2016-03-05 12:42:02 +0200

    сделал js для попапов

--- js/script.js
+++ js/script.js
@@ -1,17 +1,70 @@
-var linkFeedback = document.querySelector(".btn-feedback");
-var popupFeedback = document.querySelector(".modal-login");
-var closeFeedback = popupFeedback.querySelector(".modal-login-close");
-var loginFeedback = popupFeedback.querySelector(".input-user");
+var link = document.querySelector(".login");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal-login-close");
+var login = popup.querySelector(".input-user");
+var password = popup.querySelector(".input-password");
+var form = popup.querySelector(".modal-login-form");
+var storageLogin = localStorage.getItem("login");
 
-	linkFeedback.addEventListener("click", function(event) {
+var openMap = document.querySelector(".btn-driveway");
+var popupMap = document.querySelector(".content-map");
+var closeMap = popupMap.querySelector(".content-map-close");
+
+	link.addEventListener("click", function(event) {
 		event.preventDefault();
-		popupFeedback.classList.add("modal-login-show");
-		loginFeedback.focus();
+		popup.classList.add("modal-login-show");
+		
+		if (storageLogin) {
+			login.value = storageLogin;
+			password.focus();
+		} else {
+			login.focus();
+		}
 	});
 	
-	closeFeedback.addEventListener("click", function(event) {
+	close.addEventListener("click", function(event) {
 		event.preventDefault();
-		popupFeedback.classList.remove("modal-login-show");
+		popup.classList.remove("modal-login-show");
+		popup.classList.remove("modal-error");
+	});
+	
+	form.addEventListener("submit", function(event) {
+		if (!login.value || !password.value) {
+			event.preventDefault();
+			popup.classList.remove("modal-error");
+			popup.offsetWidth = popup.offsetWidth;
+			popup.classList.add("modal-error");
+			console.log("Вам нужно ввести логин и пароль");
+		} else {
+			localStorage.setItem("login", login.value);
+		}	
+	});
+	
+	window.addEventListener("keydown", function(event) {
+		if (event.keyCode === 27 ) {
+			if(popup.classList.contains("modal-login-show")) {
+				popup.classList.remove("modal-login-show");
+				popup.classList.remove("modal-error");
+			}
+		}
+	});
+	
+	openMap.addEventListener("click", function(event) {
+		event.preventDefault();
+		popupMap.classList.add("content-map-show");
+	});
+	
+	closeMap.addEventListener("click", function(event) {
+		event.preventDefault();
+		popupMap.classList.remove("content-map-show");
+	});
+	
+	window.addEventListener("keydown", function(event) {
+		if (event.keyCode === 27 ) {
+			if(popupMap.classList.contains("content-map-show")) {
+				popupMap.classList.remove("content-map-show");
+			}
+		}
 	});
 
-console.log(loginFeedback);  
+console.log(openMap);  
